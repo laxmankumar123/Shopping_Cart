9634d149d899208e4539d531bf41f0dc364ccd7a
laxmankumar123 <laxmankumar071997@gmail.com>
2024-08-16 00:20:20 +0530

    till_15_aut_at_11pm

--- src/main/java/com/ecom/controller/AdminController.java
+++ src/main/java/com/ecom/controller/AdminController.java
@@ -1,13 +1,24 @@
 package com.ecom.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ecom.model.Category;
+import com.ecom.service.CategoryService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 	
+	
+	@Autowired
+	private CategoryService categoryService;
 	@GetMapping("/")
 	public String index() {
 		return "admin/index";
@@ -25,6 +36,18 @@ public class AdminController {
 	@GetMapping("/category")
 	public String category() {
 		return "admin/category";
+	}
+	
+	
+
+	@PostMapping("/saveCategory")
+	public String saveCategory(@ModelAttribute Category category, HttpSession session) {
+		Boolean existCategory= categoryService.existCategory(category.getName());
+		
+		
+		
+		categoryService.saveCategory(category);
+		return "redirect:/category";
 	}
 	
 	
